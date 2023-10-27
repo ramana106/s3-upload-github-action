ecae794227b3f46e1a7e86e0af79cc4aae0c852d
Ram <ramana.nathan106@gmail.com>
2023-10-27 23:16:16 +0530

    add all files to s3

--- entrypoint.sh
+++ entrypoint.sh
@@ -32,8 +32,7 @@ echo "[default]
 aws_access_key_id = ${AWS_ACCESS_KEY_ID}
 aws_secret_access_key = ${AWS_SECRET_ACCESS_KEY}" > ~/.aws/credentials
 
-aws s3 cp ${FILE} s3://${S3_BUCKET}/${S3_KEY} \
-  --region ${AWS_REGION} $*
+aws s3 cp ./ s3://${S3_BUCKET}/${S3_KEY} --region ${AWS_REGION} --recursive $*
 
 rm -rf ~/.aws
 
